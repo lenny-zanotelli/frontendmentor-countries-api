169a2130f663bcb7c7cef5a086b9cee7502a9b9b
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-08-25 17:06:13 +0200

    feat: header component create

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './styles.scss';
+import { HiOutlineMoon } from 'react-icons/hi';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Where in the world ?</h1>
-
+      <p className="header__theme">
+        <HiOutlineMoon />
+        Dark Mode
+      </p>
     </header>
   );
 }
