68a9b82d70c743006cba12a5bd7a19c7ebdafbcf
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-10-13 15:46:53 +0200

    :sparkles: feat: add spinner for loading state, dark mode for it wip

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { useState } from 'react';
+import { CSSProperties, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import './styles.scss';
 import { Country } from '../../@types/countries';
@@ -8,6 +8,15 @@ import Layout from '../../components/Layout';
 import SearchBar from '../../components/SearchBar';
 import FilterBar from '../../components/FilterBar';
 import CardResults from '../../components/CardResults';
+import { CircleLoader } from 'react-spinners';
+
+const override: CSSProperties = {
+  display: "block",
+  position: 'fixed',
+  zIndex: '1',
+  top: '50%',
+  left: '50%',
+};
 
 function Home() {
   const [textToSearch, setTextToSearch] = useState('');
@@ -23,7 +32,11 @@ function Home() {
 
   if (isLoading) {
     console.log('Loading...');
-    return <div>Loading...</div>;
+    return <CircleLoader
+    cssOverride={override} 
+    aria-label='Loading Spinner' 
+    data-testid='loader' 
+    />;
   }
 
   if (isError) {
