81b93a4246ef1d16133f95306ecba181cded08c1
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-10-20 16:40:04 +0200

    :lipstick: style(details): add class and new span markups and add css for content

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -5,13 +5,15 @@ import { useQuery } from '@tanstack/react-query';
 import { CSSProperties, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Country } from '../../@types/countries';
+import { cca3ToNameMap } from '../../@types/cca3ToNameMap';
 import { getOneCountry } from '../../services/getOneCountry';
-import './styles.scss';
 import Layout from '../../components/Layout';
 import BackButton from '../../components/BackButton';
 import formatNumber from '../../utils/formatPopNumber';
-import { cca3ToNameMap } from '../../@types/cca3ToNameMap';
 import CircleLoader from 'react-spinners/CircleLoader';
+
+import './styles.scss';
+
 
 const override: CSSProperties = {
   display: "block",
@@ -57,47 +59,141 @@ function Detail() {
   return (
     <Layout>
       <BackButton />
-      <section>
-        <img src={country.flags.png} alt={country.name.common} />
-        <h1>{country.name.common}</h1>
-        <p>
-          Population:
-          {country?.population ? formatNumber(country?.population) : ''}
-        </p>
-        <p>
-          Region:
-          {country.region}
-        </p>
-        <p>
-          Sub Region:
-          {country.subregion}
-        </p>
-        <p>
-          Capital :
-          {country.capital}
-        </p>
-        <p>
-          Top Level Domain :
-          {country.tld}
-        </p>
-        <p>
-          Currencies: 
-          {Object.keys(country.currencies)
-          .map((currencyCode, index, array) => (
-            <span key={currencyCode}>
-              {country.currencies[currencyCode].name}
-              {index !== array.length - 1 ? ', ' : ''}
+      <section 
+        className='container__details'
+        >
+        {/* --- IMAGE --- */}
+        <div 
+          className='container__img'
+          >
+          <img 
+            src={country.flags.png} 
+            alt={country.name.common} 
+          />
+        </div>
+        {/* --- TOP CONTENT --- */}
+        <div
+          className='container__top-content'
+        >
+          <h1
+            className='content__title'
+          >
+            {' '}
+            {country.name.common}
+          </h1>
+          <p 
+            className='content__element--title'
+          >
+            Native Name:
+            {' '}
+            {Object.entries(country.name.nativeName)
+              .map(([key, translation], index, array) => (
+              <span
+                className='element--information' 
+                key={key}
+              >
+                {translation.common}
+                {index !== array.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+          </p>
+          <p 
+            className='content__element--title'
+          >
+            Population:
+            <span className='element--information' >
+              {' '}
+              {country?.population ? formatNumber(country?.population) : ''}
             </span>
-          ))}
-        </p>
-        <p>
-          Languages : {Object.values(country.languages).join(', ')}
-
-        </p>
-        <div>
-          <h3>Border Countries :</h3>
+          </p>
+          <p 
+            className='content__element--title'
+          >
+            Region:
+            <span className='element--information'
+            >
+              {' '}
+              {country.region}
+            </span>
+          </p>
+          <p 
+            className='content__element--title'
+          >
+            Sub Region:
+            <span 
+            className='element--information'
+            >
+              {' '}
+              {country.subregion}
+            </span>
+          </p>
+          <p 
+          className='content__element--title'
+          >
+            Capital:
+            <span 
+              className='element--information'
+            >
+              {' '}
+              {country.capital}
+            </span>
+          </p>
+        </div>
+        {/* --- BOTTOM CONTENT --- */}
+        <div 
+        className='container__bottom-content'
+        >
+          <p 
+          className='content__element--title'
+          >
+            Top Level Domain:
+            <span 
+              className='element--information'
+            >
+              {' '}
+              {country.tld}
+            </span>
+          </p>
+          <p 
+          className='content__element--title'
+          >
+            Currencies:
+            {' '} 
+            {Object.keys(country.currencies)
+            .map((currencyCode, index, array) => (
+              <span
+                className='element--information'  
+                key={currencyCode}
+              >
+                {country.currencies[currencyCode].name}
+                {index !== array.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+          </p>
+          <p 
+          className='content__element--title'
+          >
+            Languages: 
+            <span
+              className='element--information' 
+            >
+              {' '}
+              {Object.values(country.languages).join(', ')}
+            </span>
+          </p>
+        </div>
+        {/* --- BORDER COUNTRIES CONTENT --- */}
+        <div
+          className='container__border-content'  
+        >
+          <h3 
+          className='border-content__subtitle'
+          >
+            Border Countries:
+          </h3>
           {country?.borders?.map((border, index) => (
             <button
+              className='border-content__country'
               type="button"
               key={index}
             >
