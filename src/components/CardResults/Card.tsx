dbcb4b7c1ff581e0266b71558e396518f1ef0578
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-12-18 11:56:05 +0100

    refactor: change current card to radix ui cards

--- src/components/CardResults/Card.tsx
+++ src/components/CardResults/Card.tsx
@@ -2,48 +2,59 @@
 /* eslint-disable no-nested-ternary */
 import { Link } from 'react-router-dom';
 import { Country } from '../../@types/countries';
-import './styles.scss';
+import { Card, Flex, Heading, Inset, Strong, Text } from '@radix-ui/themes';
 import formatNumber from '../../utils/formatPopNumber';
+import './styles.scss';
 
 interface CountryProps {
   country: Country,
 }
 
-function Card({ country }: CountryProps) {
+function CardComponent({ country }: CountryProps) {
   const populationFormat = formatNumber(country.population);
   return (
-    <Link
-      to={`/country/${country.cca3}`}
-    >
-      <div className="card">
-        <img src={country.flags.png} alt={country.name.common} />
-        <h2 className="card-title">{country.name.common}</h2>
-        <p className="card-element">
-          Population :
-          {' '}
-          {populationFormat}
-        </p>
-        <p className="card-element">
-          Region :
-          {country.region}
-        </p>
-        <p className="card-element">
-          {/* TODO: Can i do a better code than nested ternary for join multiple capitals here ? */}
-          Capital :
-          {' '}
-          {country.capital ? (country.capital.length >= 2 ? (
-            country.capital.join(', ')
-          ) : (
-            country.capital[0]
-          )
-          ) : (
-            'N/A'
-          )}
-        </p>
 
-      </div>
-    </Link>
+      <Card asChild size='2' style={{ maxWidth: 240}} variant='surface' >
+        <Link to={`/country/${country.cca3}`}>
+          <Flex gap='3' justify='between' direction='column'>
+            <Inset pb='current'>
+
+              <img 
+              src={country.flags.png} 
+              alt={country.name.common}
+              style={{
+                display: 'block',
+                objectFit: 'cover',
+                width: '100%',
+                height: 140,
+              }}
+              />
+            </Inset>
+            <Heading as='h2' weight='bold'>{country.name.common}</Heading>
+            <Text as='p'>
+              <Strong>Population:</Strong> {populationFormat}
+            </Text>
+            <Text as='p'>
+              Region : {country.region}
+            </Text>
+            <Text as='p'>
+              {/* TODO: Can i do a better readable code than a nested ternary for join multiple capitals here ? */}
+              Capital :
+              {country.capital ? (country.capital.length >= 2 ? (
+                country.capital.join(', ')
+              ) : (
+                country.capital[0]
+              )
+              ): (
+                'N/A'
+              )}
+            </Text>
+          </Flex>
+        </Link>  
+
+      </Card>
+
   );
 }
 
-export default Card;
+export default CardComponent;
