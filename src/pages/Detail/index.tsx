e1e462f2a9bce177b9c149f4b53f2940e3f59937
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-12-22 19:05:36 +0100

    refactor: change detail page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,3 +1,4 @@
+import { Flex } from '@radix-ui/themes';
 import Layout from '../../components/Layout';
 import BackButton from './partials/BackButton';
 import ContentDetail from './partials/ContentDetail';
@@ -8,8 +9,15 @@ function Detail() {
   return (
     <Layout>
 
+      <Flex 
+        direction='column' 
+        width='max-content'
+        gap='9'
+      >
         <BackButton />
         <ContentDetail />
+      </Flex>
+
     </Layout>
   );
 }
--- src/pages/Detail/partials/ContentDetail/index.tsx
+++ src/pages/Detail/partials/ContentDetail/index.tsx
@@ -3,9 +3,9 @@ import { Link, useParams } from "react-router-dom";
 import { getOneCountry } from "../../../../services/getOneCountry";
 import { useEffect } from "react";
 import { Country } from "../../../../@types/countries";
-import Loader from "../../../../components/Loader";
-import { Box, Button, Flex, Heading, Strong, Text } from "@radix-ui/themes";
 import { cca3ToNameMap } from "../../../../@types/cca3ToNameMap";
+import Loader from "../../../../components/Loader";
+import { AspectRatio, Box, Button, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 
 
 
@@ -38,87 +38,142 @@ function ContentDetail() {
     return <div>Country not found</div>;
   }
   return(
-    <Flex>
-      <img
-        src={country.flags.svg}
-        alt={country.name.common}
-      />
-
-      <Heading as='h1'>
-        {country.name.common}
-      </Heading>
-
-      <Text as='p'>
-        Native Name:
-        {Object.entries(country.name.nativeName)
-          .map(([key, translation], index, array) => (
-          <Text as='span' key={key}>
-            {translation.common}
-            {index !== array.length -1 ? ', ' : ''}
-          </Text>
-        ))}
-      </Text>
-
-      <Text as='p'>
-        Region:
-        <Text as='span'>{country.region}</Text>
-      </Text>
-
-      <Text as='p'>
-        Sub Region:
-        <Text as='span'>{country.subregion}</Text>
-      </Text>
-
-      <Text as='p'>
-      Capital:
-        <Text as='span'>{country.capital}</Text>
-      </Text>
-
-      <Text as='p'>
-        Top Level Domain:
-        <Text as='span'>{country.tld}</Text>
-      </Text>
-
-      <Text as='p'>
-        Currencies:
-        {Object.keys(country.currencies)
-          .map((currencyCode, index, array) => (
-            <Text as='span' key={currencyCode}>
-              {country.currencies[currencyCode].name}
-              {index !== array.length - 1 ? ', ' : ''}
-            </Text>
-          ))}
-      </Text>
-
-      <Text as='p'>
-        Languages:
-        <Text as='span'>
-          {Object.values(country.languages).join(', ')}
-        </Text>
-      </Text>
-      <Box>
-        <Heading as='h3'>Border Countries:</Heading>
-
-        {country?.borders?.map((border, index) => (
-          <Button
-            key={index}
-          >
-            <Link to={`/country/${border}`}>
-              {cca3ToNameMap[border]}
-            </Link>
-
-          </Button>
-        ))}
-        
+    <Grid 
+      columns='2' 
+      gap='9'
+      justify='center' 
+      style={{ 
+        width: 1500
+      }}
+    >
+      <Box width='auto' height='auto'>
+          <img
+            src={country.flags.svg}
+            alt={country.name.common}
+            style={{
+              objectFit: 'contain',
+              width: '100%',
+              height: '100%',
+            }}
+          />
       </Box>
 
+      <Flex direction='column' mt='9'>
+        <Box width='100%' height='max-content' mb='9'>
+          <Heading as='h1' size='8'>
+            {country.name.common}
+          </Heading>
+        </Box>
 
+        <Flex direction='row'>
 
+            <Flex 
+              direction='column'
+              width='100%' 
+              justify='center'
+              gap='3'
+              mb='9'
+            >
 
+              <Text as='p' weight='bold'>
+                Native Name:
+                {Object.entries(country.name.nativeName)
+                  .map(([key, translation], index, array) => (
+                  <Text 
+                    as='span'
+                    weight='regular'
+                    key={key}
+                  >
+                    &nbsp;
+                    {translation.common}
+                    {index !== array.length -1 ? ', ' : ''}
+                  </Text>
+                ))}
+              </Text>
 
+              <Text as='p' weight='bold'>
+                Region:
+                <Text as='span' weight='regular'>&nbsp;{country.region}</Text>
+              </Text>
 
-      
-    </Flex>
+              <Text as='p' weight='bold'>
+                Sub Region:
+                <Text as='span' weight='regular'>&nbsp;{country.subregion}</Text>
+              </Text>
+
+              <Text as='p' weight='bold'>
+              Capital:
+                <Text as='span' weight='regular'>&nbsp;{country.capital}</Text>
+              </Text>
+
+            </Flex>
+
+          
+          <Flex 
+            direction='column' 
+            align='start'
+            width='100%'
+            gap='3'
+          >
+            <Text as='p' weight='bold'>
+              Top Level Domain:
+              <Text as='span' weight='regular'>&nbsp;{country.tld}</Text>
+            </Text>
+
+            <Text as='p' weight='bold'>
+              Currencies:
+              {Object.keys(country.currencies)
+                .map((currencyCode, index, array) => (
+                  <Text 
+                    as='span'
+                    weight='regular'
+                    key={currencyCode}
+                  >
+                    &nbsp;
+                    {country.currencies[currencyCode].name}
+                    {index !== array.length - 1 ? ', ' : ''}
+                  </Text>
+                ))}
+            </Text>
+
+            <Text as='p' weight='bold'>
+              Languages:
+              <Text as='span' weight='regular'>
+                &nbsp;
+                {Object.values(country.languages).join(', ')}
+              </Text>
+            </Text>
+          </Flex>
+        </Flex>
+
+          <Flex 
+            justify='start' 
+            align='center'
+            wrap='wrap'
+            gap='2'
+          >
+
+            <Heading as='h3' size='3'>Border Countries:</Heading>
+            {country?.borders?.map((border, index) => (
+   
+              <Button
+                size='2'
+                variant='surface'
+                color='gray'
+                highContrast
+                key={index}
+              >
+                <Link to={`/country/${border}`}>
+                  {cca3ToNameMap[border]}
+                </Link>
+              </Button>
+
+            ))}
+            
+          </Flex>
+      </Flex>
+
+    </Grid>
 
   );
 }
