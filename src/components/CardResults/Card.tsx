1e9aca3194badd320b6bd9d5c39d85c986b443ae
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-12-23 20:21:02 +0100

    feat(home): responsive for mobile

--- src/components/CardResults/Card.tsx
+++ src/components/CardResults/Card.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Country } from '../../@types/countries';
-import { Card, Flex, Heading, Inset, Strong, Text } from '@radix-ui/themes';
+import { AspectRatio, Card, Flex, Heading, Inset, Strong, Text } from '@radix-ui/themes';
 import formatNumber from '../../utils/formatPopNumber';
 
 interface CountryProps {
@@ -11,46 +11,59 @@ function CardComponent({ country }: CountryProps) {
   const populationFormat = formatNumber(country.population);
   return (
 
-      <Card asChild size='4' style={{ maxWidth: 450 }} variant='surface' >
+      <Card
+        asChild
+        size='3'
+        variant='classic'
+        style={{ maxWidth: 300 }}
+      >
         <Link to={`/country/${country.cca3}`}>
-          <Flex 
-            gap='3' 
-            justify='between' 
+          <Flex
+            gap='5'
             direction='column'
           >
             <Inset pb='current'>
-              <img 
-              src={country.flags.png} 
-              alt={country.name.common}
-              style={{
-                display: 'block',
-                objectFit: 'cover',
-                width: '100%',
-                height: 180,
-                marginBottom: '1rem '
-              }}
-              />
+              <AspectRatio ratio={2}>
+                <img 
+                src={country.flags.svg} 
+                alt={country.name.common}
+                style={{
+                  objectFit: 'cover',
+                  width: '100%',
+                  height: '100%',
+                }}
+                />
+              </AspectRatio>
             </Inset>
-            <Heading as='h2' weight='bold'>{country.name.common}</Heading>
-            <Flex gap='1' justify='center' direction='column'>
-            <Text as='p'>
-              <Strong>Population: </Strong> {populationFormat}
-            </Text>
-            <Text as='p'>
-              <Strong>Region: </Strong> {country.region}
-            </Text>
-            <Text as='p'>
-              {/* TODO: Can i do a better readable code than a nested ternary for join multiple capitals here ? */}
-              <Strong>Capital: </Strong>
-              {country.capital ? (country.capital.length >= 2 ? (
-                country.capital.join(', ')
-              ) : (
-                country.capital[0]
-              )
-              ): (
-                'N/A'
-              )}
-            </Text>
+            <Flex 
+              direction='column'
+              width='100%'
+            >
+            <Heading 
+              as='h2' 
+              mb='4' 
+              weight='bold'
+            >
+              {country.name.common}
+            </Heading>
+              <Text as='p'>
+                <Strong>Population: </Strong> {populationFormat}
+              </Text>
+              <Text as='p'>
+                <Strong>Region: </Strong> {country.region}
+              </Text>
+              <Text as='p'>
+                {/* TODO: Can i do a better readable code than a nested ternary for join multiple capitals here ? */}
+                <Strong>Capital: </Strong>
+                {country.capital ? (country.capital.length >= 2 ? (
+                  country.capital.join(', ')
+                ) : (
+                  country.capital[0]
+                )
+                ): (
+                  'N/A'
+                )}
+              </Text>
 
             </Flex>
           </Flex>
