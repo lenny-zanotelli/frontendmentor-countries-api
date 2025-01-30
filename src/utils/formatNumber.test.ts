c853acc9e7ee1ade9b9962292000558174994014
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2025-01-30 13:04:19 +0100

    test(formatNumber): add unit test

--- src/utils/formatNumber.test.ts
+++ src/utils/formatNumber.test.ts
@@ -1,5 +1,11 @@
+import formatNumber from './formatNumber';
 
-
-describe('Format Number function' ,() => {
-  it('should')
-})
+describe('Format Number function', () => {
+  it('should return something', () => {
+    expect(formatNumber).toBeDefined();
+  });
+  it('should return a string with en-US format for big number', () => {
+    const number = 123456789;
+    expect(formatNumber(number)).toBe('123,456,789');
+  });
+});
